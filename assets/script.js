2f6da974dd791fe13d8ff3a100950c6d147984cd
Aharon-zerbib <120047107+Aharon-zerbib@users.noreply.github.com>
2023-08-10 17:07:40 +0200

    2eme
    commit2

--- assets/script.js
+++ assets/script.js
@@ -19,77 +19,76 @@ const slides = [
 ];
 
 // Create the banner with JS
-document.addEventListener("DOMContentLoaded", function () {
-  let bannerDiv = document.querySelector("#banner");
-  let basseImg = 0;
 
-  //intégration de la flèche  de gauche
-  let arrowLeft = document.createElement("img");
-  arrowLeft.classList.add("arrow", "arrow_left");
-  arrowLeft.src = "./assets/images/arrow_left.png";
-  //function sur le click left
-  arrowLeft.addEventListener("click", function () {
-    basseImg = (basseImg - 1 + slides.length) % slides.length;
-    updateBanner();
-  });
+let bannerDiv = document.querySelector("#banner");
+let basseImg = 0;
 
-  let bannerImg = document.createElement("img");
-  bannerImg.className = "banner-img";
-  bannerImg.src = "./assets/images/slideshow/" + slides[basseImg].image;
-
-  let bannerTagline = document.createElement("p");
-  bannerTagline.innerHTML = slides[basseImg].tagLine;
-
-  //intégration de la flèche  de droite
-  let arrowRightImg = document.createElement("img");
-  arrowRightImg.classList.add("arrow", "arrow_right");
-  arrowRightImg.src = "./assets/images/arrow_right.png";
-  //function sur le click riggth
-  arrowRightImg.addEventListener("click", function () {
-    basseImg = (basseImg + 1) % slides.length;
-    updateBanner();
-  });
-
-  let dotsImg = document.createElement("div");
-  dotsImg.className = "dots";
-
-  function updateBanner() {
-    bannerImg.src = "./assets/images/slideshow/" + slides[basseImg].image;
-
-    if (bannerTagline) {
-      bannerDiv.removeChild(bannerTagline);
-    }
-
-    bannerTagline = document.createElement("p");
-    bannerTagline.innerHTML = slides[basseImg].tagLine;
-
-    bannerDiv.appendChild(bannerTagline);
-
-    dots.forEach((dot, index) => {
-      dot.classList.toggle("dot_selected", index === basseImg);
-    });
-  }
-
-  // Ajoute éléments a bannerGiv
-  bannerDiv.appendChild(arrowLeft);
-  bannerDiv.appendChild(bannerImg);
-  bannerDiv.appendChild(bannerTagline);
-  bannerDiv.appendChild(arrowRightImg);
-  bannerDiv.appendChild(dotsImg);
-
-  // la function sur le dots
-  let dots = [];
-  slides.forEach((_slide, index) => {
-    let dot = document.createElement("span");
-    dot.classList.add("dot");
-    dot.addEventListener("click", function () {
-      basseImg = index;
-      updateBanner();
-    });
-
-    dotsImg.appendChild(dot);
-    dots.push(dot);
-  });
-
+//intégration de la flèche  de gauche
+let arrowLeft = document.createElement("img");
+arrowLeft.classList.add("arrow", "arrow_left");
+arrowLeft.src = "./assets/images/arrow_left.png";
+//function sur le click left
+arrowLeft.addEventListener("click", function () {
+  basseImg = (basseImg - 1 + slides.length) % slides.length; // calcule reste division de la valeur précédente
   updateBanner();
 });
+
+let bannerImg = document.createElement("img");
+bannerImg.className = "banner-img";
+bannerImg.src = "./assets/images/slideshow/" + slides[basseImg].image;
+
+let bannerTagline = document.createElement("p");
+bannerTagline.innerHTML = slides[basseImg].tagLine;
+
+//intégration de la flèche  de droite
+let arrowRightImg = document.createElement("img");
+arrowRightImg.classList.add("arrow", "arrow_right");
+arrowRightImg.src = "./assets/images/arrow_right.png";
+//function sur le click riggth
+arrowRightImg.addEventListener("click", function () {
+  basseImg = (basseImg + 1) % slides.length;
+  updateBanner();
+});
+
+let dotsImg = document.createElement("div");
+dotsImg.className = "dots";
+
+function updateBanner() {
+  bannerImg.src = "./assets/images/slideshow/" + slides[basseImg].image;
+
+  if (bannerTagline) {
+    bannerDiv.removeChild(bannerTagline); //si exsite alors parent suprim bannerDiv
+  }
+
+  bannerTagline = document.createElement("p");
+  bannerTagline.innerHTML = slides[basseImg].tagLine;
+
+  bannerDiv.appendChild(bannerTagline);
+
+  dots.forEach((dot, index) => {
+    dot.classList.toggle("dot_selected", index === basseImg);
+  });
+}
+
+// Ajoute éléments a bannerGiv
+bannerDiv.appendChild(arrowLeft);
+bannerDiv.appendChild(bannerImg);
+bannerDiv.appendChild(bannerTagline);
+bannerDiv.appendChild(arrowRightImg);
+bannerDiv.appendChild(dotsImg);
+
+// la function sur le dots
+let dots = [];
+slides.forEach((_slide, index) => {
+  let dot = document.createElement("span");
+  dot.classList.add("dot");
+  dot.addEventListener("click", function () {
+    basseImg = index;
+    updateBanner();
+  });
+
+  dotsImg.appendChild(dot);
+  dots.push(dot);
+});
+
+updateBanner();
